ef20f7621ccb626460e50e05149cdf15edcd9e1b
olivermanzi <olivermanzi.dev@gmail.com>
2021-06-21 13:23:11 +0200

    adds picture layout

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,58 @@
 <template>
-  <div>
-    <b-container
-      id="container"
-      fluid>
-      <b-navbar
-        toggleable="sm"
-        sticky>
-        <b-navbar-brand class="brand">
-          Sam Druant
-        </b-navbar-brand>
+  <div id="container">
+    <b-navbar toggleable="sm">
+      <b-navbar-brand class="brand">
+        Sam Druant
+      </b-navbar-brand>
 
-        <b-navbar-toggle target="sidebar-nav" />
-        <b-collapse
-          id="nav"
-          is-nav>
-          <b-navbar-nav
-            class="ml-auto"
-            align="end">
-            <!-- links -->
-            <b-nav-form>
-              <b-link class="mx-2 simple-link">
-                Work
-              </b-link>
-              <b-link class="mx-2 simple-link">
-                About
-              </b-link>
-            </b-nav-form>
-          </b-navbar-nav>
-        </b-collapse>
-      </b-navbar>
+      <b-navbar-toggle target="sidebar-nav" />
+      <b-collapse
+        id="nav"
+        is-nav>
+        <b-navbar-nav
+          class="ml-auto"
+          align="end">
+          <!-- links -->
+          <b-nav-form>
+            <b-link class="mx-2 simple-link">
+              Works
+            </b-link>
+            <b-link class="mx-2 simple-link">
+              About
+            </b-link>
+          </b-nav-form>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    
+    <b-row
+      id="content"
+      align-h="center"
+      align-v="stretch">
+      <b-col cols="12">
+        <router-view />
+      </b-col>
+    </b-row>
 
-      <b-row
-        id="content"
-        align-h="center"
-        align-v="stretch">
-        <b-col cols="12">
-          <router-view />
-        </b-col>
-      </b-row>
-
-      <b-row
-        id="footer"
-        class="mb-3"
-        align-h="center">
-        <b-col
-          cols="auto"
-          class="mx-1">
-          <img
-            src="./assets/svgs/instagram.svg"
-            width="auto"
-            height="20px"
-            style="color: #000000;">
-        </b-col>
-        <b-col
-          cols="auto"
-          class="mx-1">
-          sam@mail.com
-        </b-col>
-      </b-row>
-    </b-container>
+    <b-row
+      id="footer"
+      class="mb-3"
+      align-h="center">
+      <b-col
+        cols="auto"
+        class="mx-1">
+        <img
+          src="./assets/svgs/instagram.svg"
+          width="auto"
+          height="20px"
+          style="color: #000000;">
+      </b-col>
+      <b-col
+        cols="auto"
+        class="mx-1">
+        sam@mail.com
+      </b-col>
+    </b-row>
   </div>
 </template>
 
---
+++ src/components/views/PictureFrame.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <b-row align-h="center">
+      <b-col cols="auto">
+        <b-img :src="src" />
+      </b-col>
+      <b-col cols="12" />
+      <b-col cols="auto">
+        {{ `${title}, (${size}), ${material}` }}
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+	export default {
+		name: "PictureFrame",
+		props:{
+			title: {
+				type: String,
+				required: true
+			},
+			src: {
+				type: String,
+				required: true
+			},
+			material: {
+				type: String,
+				required: true
+			},
+			size: {
+				type: String,
+				required: true
+			}
+		}
+	};
+</script>
