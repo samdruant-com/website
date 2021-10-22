2ae220023b5bf8056bcc9216cc1c51a0e0214b9c
olivermanzi <olivermanzi.dev@gmail.com>
2021-10-22 14:36:50 +0200

    styles footer and contact page

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,22 +1,16 @@
 <template>
 	<v-footer
 		app
-		padless
 		color="transparent">
-		<v-row
-			class="mx-2 my-2"
-			justify="center">
-			<v-col cols="auto">
-				<a
-					class="link"
-					target="_blank"
-					href="https://www.instagram.com/samdruant">
-					<img
-						height="15"
-						src="~/assets/images/icons/instagram.webp">
-				</a>
-			</v-col>
-		</v-row>
+		<div class="my-2 mr-auto ml-auto">
+			<a
+				target="_blank"
+				href="https://www.instagram.com/samdruant">
+				<img
+					height="15"
+					src="~/assets/images/icons/instagram.webp">
+			</a>
+		</div>
 	</v-footer>
 </template>
 
