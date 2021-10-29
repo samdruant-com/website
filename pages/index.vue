c9fa1d8c1584a28d0ad554ac3812d6c5034a170c
olivermanzi <olivermanzi.dev@gmail.com>
2021-10-29 21:05:57 +0200

    adds sams picture again

--- pages/index.vue
+++ pages/index.vue
@@ -19,7 +19,7 @@ export default {
 <style scoped>
 #start-page {
 	height: 100vh;
-	background-size: cover;
+	background-size:cover;
 	background-color: #cccccc;
 	background-repeat: no-repeat;
 	background-image: url('~assets/images/img-1.webp');
