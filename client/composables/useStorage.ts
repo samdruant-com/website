1faba1658af121be46251f59f9bd0e206cf808ef
Oliver Manzi <olivermanzi.dev@gmail.com>
2023-12-01 22:53:50 +0100

    (v2.8.0) feature: client adds support for cookies as local storage

--- client/composables/useStorage.ts
+++ client/composables/useStorage.ts
@@ -1,30 +1,55 @@
-export function useStorage() {
-   
-  function set(key: string, value: string): boolean {
-    try {
-      localStorage.setItem(key, value);
-      return true;
-    } catch (error) {
-      return false;
-    }
-  }
+interface StorageOptions {
+  server?: boolean;
+  cookie?: boolean;
+}
 
-  function get(key: string): string | null {
-    try {
-      return localStorage.getItem(key);
-    } catch (error) {
-      return null;
-    }
-  }
+export function useStorage () {
+	function set (key: string, value: string, options?: StorageOptions): boolean {
+		try {
+			if (options?.server || options?.cookie || process.server) {
+				const cookie = useCookie(key);
+				cookie.value = value;
+			} else {
+				localStorage.setItem(key, value);
+			}
 
-  function remove(key: string): boolean {
-    try {
-      localStorage.removeItem(key);
-      return true;
-    } catch (error) {
-      return false;
-    }
-  }
+			return true;
+		} catch (error) {
+			return false;
+		}
+	}
 
-  return { set, get, remove };
+	function get (key: string, options?: StorageOptions): string | null {
+		try {
+			let value = null;
+
+			if (options?.server || options?.cookie || process.server) {
+				const cookie = useCookie(key);
+				value = cookie.value as unknown as string | null;
+			} else {
+				value = localStorage.getItem(key);
+			}
+
+			return value;
+		} catch (error) {
+			return null;
+		}
+	}
+
+	function remove (key: string, options?: StorageOptions): boolean {
+		try {
+			if (options?.server || options?.cookie || process.server) {
+				const cookie = useCookie(key);
+				cookie.value = null;
+			} else {
+				localStorage.removeItem(key);
+			}
+
+			return true;
+		} catch (error) {
+			return false;
+		}
+	}
+
+	return { set, get, remove };
 }
