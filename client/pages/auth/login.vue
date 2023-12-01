d82bfd39afa468de07bd12fded5c2d86701366ba
Oliver Manzi <olivermanzi.dev@gmail.com>
2023-12-01 23:09:46 +0100

    (v2.9.2) patch: client makes auth form transparent

--- client/pages/auth/login.vue
+++ client/pages/auth/login.vue
@@ -5,31 +5,24 @@ const router = useRouter();
 
 <template>
   <base-page>
-    <v-sheet>
+    <base-card>
       <v-row justify="center">
-        <v-col
-          class="pa-1"
-          cols="auto">
+        <v-col class="pa-1" cols="auto">
           <h2>Login</h2>
         </v-col>
 
         <v-divider class="border-opacity-0" />
 
-        <v-col
-          class="pa-1"
-          cols="10"
-          md="7">
+        <v-col class="pa-1" cols="10" md="7">
           <AuthForm mode="login" @login="router.push('/')" />
         </v-col>
 
         <v-divider class="border-opacity-0" />
 
-        <v-col
-          class="pa-1"
-          cols="auto">
+        <v-col class="pa-1" cols="auto">
           Don't have an account? Register <NuxtLink to="/auth/register">here</NuxtLink>.
         </v-col>
       </v-row>
-    </v-sheet>
+    </base-card>
   </base-page>
 </template>
