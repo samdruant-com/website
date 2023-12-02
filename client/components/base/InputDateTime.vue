5ebb46c6e4b77cd19abf50ead383a5456afa6262
Oliver Manzi <olivermanzi.dev@gmail.com>
2023-12-02 13:20:58 +0100

    (v2.10.2) fix: client InputDateTime was returning Nan values

--- client/components/base/InputDateTime.vue
+++ client/components/base/InputDateTime.vue
@@ -1,102 +1,105 @@
 <script setup lang="ts">
-import DayJs from "dayjs";
+import DayJs from 'dayjs';
 
 const props = defineProps({
-	modelValue: {
-		type: [Number, String],
-		default: undefined,
-	},
-	label: {
-		type: String,
-		default: "",
-	},
-	labelDate: {
-		type: String,
-		default: "Date",
-	},
-	labelTime: {
-		type: String,
-		default: "Time",
-	},
-	hideDate: {
-		type: Boolean,
-		default: false,
-	},
-	hideTime: {
-		type: Boolean,
-		default: false,
-	},
-	color: {
-		type: String,
-		default: "transparent",
-	},
+  modelValue: {
+    type: [Number, String],
+    default: undefined
+  },
+  label: {
+    type: String,
+    default: ''
+  },
+  labelDate: {
+    type: String,
+    default: 'Date'
+  },
+  labelTime: {
+    type: String,
+    default: 'Time'
+  },
+  hideDate: {
+    type: Boolean,
+    default: false
+  },
+  hideTime: {
+    type: Boolean,
+    default: false
+  },
+  color: {
+    type: String,
+    default: 'transparent'
+  }
 });
 
-const emit = defineEmits(["update:model-value"]);
+const emit = defineEmits(['update:model-value']);
 
 const form = reactive({
-	date: props.modelValue ? convertToDateTime(props.modelValue).date : "",
-	time: props.modelValue ? convertToDateTime(props.modelValue).time : "",
+  date: props.modelValue ? convertToDateTime(props.modelValue).date : '',
+  time: props.modelValue ? convertToDateTime(props.modelValue).time : ''
 });
 
 const validDate = computed<boolean>(() => {
-	return props.hideDate ? true : DayJs(form.date).isValid();
+  return props.hideDate ? true : DayJs(form.date).isValid();
 });
 
 const validTime = computed<boolean>(() => {
-	const [hour, minute] = form.time.split(":");
-	return props.hideTime
-		? true
-		: DayJs(validDate.value ? form.date : undefined)
-				.hour(Number(hour))
-				.minute(Number(minute))
-				.isValid();
+  const [hour, minute] = form.time.split(':');
+  return props.hideTime
+    ? true
+    : DayJs(validDate.value ? form.date : undefined)
+      .hour(Number(hour))
+      .minute(Number(minute))
+      .isValid();
 });
 
 const validForm = computed<boolean>(() => {
-	return validDate.value && validTime.value;
+  return validDate.value && validTime.value;
 });
 
 function convertToUnix(dateTime: { date: string; time: string }): number {
-	const [hour, minute] = dateTime.time.split(":");
-	return DayJs(dateTime.date).hour(Number(hour)).minute(Number(minute)).unix();
+  const [hour, minute] = dateTime.time.split(':');
+  return hour && minute
+    ? DayJs(dateTime.date).hour(Number(hour)).minute(Number(minute)).unix()
+    : DayJs(dateTime.date).unix();
 }
 
 function convertToDateTime(unix: number | string): {
-	date: string;
-	time: string;
+  date: string;
+  time: string;
 } {
-	if (typeof unix === "string") {
-		unix = Number(unix);
-	}
+  if (typeof unix === 'string') {
+    unix = Number(unix);
+  }
 
-	const date = DayJs.unix(unix);
-	return {
-		date: date.format("YYYY-MM-DD"),
-		time: date.format("HH:mm"),
-	};
+  const date = DayJs.unix(unix);
+  return {
+    date: date.format('YYYY-MM-DD'),
+    time: date.format('HH:mm')
+  };
 }
 
 watch(
-	() => form,
-	() => {
-		if (validForm.value) {
-			const unix: number = convertToUnix(form);
-			emit("update:model-value", unix);
-		}
-	},
-	{ deep: true }
+  () => form,
+  () => {
+    if (validForm.value) {
+      const unix: number = convertToUnix(form);
+      console.log('unix', unix);
+      emit('update:model-value', unix);
+    }
+  },
+  { deep: true }
 );
 </script>
 
 <template>
-	<v-row justify="center" no-gutters>
-		<v-col v-if="!props.hideTime" :cols="props.hideDate ? 12 : 6">
-			<InputText v-model="form.time" type="time" :label="props.labelTime" />
-		</v-col>
+  <v-row justify="center" no-gutters>
+    <v-col v-if="!props.hideTime" :cols="props.hideDate ? 12 : 6">
+      <InputText v-model="form.time" type="time" :label="props.labelTime" />
+    </v-col>
 
-		<v-col v-if="!props.hideDate" :cols="props.hideTime ? 12 : 6">
-			<InputText v-model="form.date" type="date" :label="props.labelDate" />
-		</v-col>
-	</v-row>
+    <v-col v-if="!props.hideDate" :cols="props.hideTime ? 12 : 6">
+      <InputText v-model="form.date" type="date" :label="props.labelDate" />
+    </v-col>
+  </v-row>
 </template>
