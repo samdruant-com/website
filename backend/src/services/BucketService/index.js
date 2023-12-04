eeaf4d2b597ce6e80b061386bf7abff3062a73e6
Oliver Manzi <olivermanzi.dev@gmail.com>
2023-12-04 20:05:29 +0100

    (v2.14.6) patch: removes old folders

--- backend/src/services/BucketService/index.js
+++
@@ -1,9 +0,0 @@
-import DoBucket from "./DoBucket.js";
-import LocalBucket from "./LocalBucket.js";
-import Env from "../../config/EnvConfig.js";
-
-const { NODE_ENV } = Env;
-
-const bucket = NODE_ENV === "development" ? LocalBucket : DoBucket;
-
-export default bucket;
