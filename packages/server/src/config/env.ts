fd69906f7460b345f89d80a9f98c5cc5f72e6f64
Oliver Manzi <olivermanzi.dev@gmail.com>
2024-05-08 21:45:30 +0200

    (v3.1.7) patch: fixes incorrect env file being set

--- packages/server/src/config/env.ts
+++ packages/server/src/config/env.ts
@@ -1,13 +1,12 @@
 import dotenv from "dotenv";
 
-// load base .env file
-dotenv.config();
-
 // load environment specific .env files
 if (process.env.NODE_ENV === "test") {
 	dotenv.config({ path: ".env.test" });
 } else if (process.env.NODE_ENV === "development") {
 	dotenv.config({ path: ".env.dev" });
+} else {
+	dotenv.config();
 }
 
 const NODE_ENV = process.env.NODE_ENV || "production";
@@ -18,8 +17,6 @@ const BUCKET_S3_URI: string = process.env.BUCKET_S3_URI as string;
 const BUCKET_S3_KEY: string = process.env.BUCKET_S3_KEY as string;
 const BUCKET_S3_SECRET: string = process.env.BUCKET_S3_SECRET as string;
 const BUCKET_S3_REGION: string = process.env.BUCKET_S3_REGION as string;
-//const BUCKET_GCP_ID: string = process.env.BUCKET_GCP_ID as string;
-//const BUCKET_GCP_KEY_PATH: string = process.env.BUCKET_GCP_KEY_PATH as string;
 const ALLOWED_ORIGINS: string = process.env.ALLOWED_ORIGINS as string;
 const JWT_SECRET: string = process.env.JWT_SECRET as string;
 const ADMIN_SECRET: string = process.env.ADMIN_SECRET as string;
