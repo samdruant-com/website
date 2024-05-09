ce38561b0e87f7eb9955f73e6018f5330bce7e5e
Oliver Manzi <olivermanzi.dev@gmail.com>
2024-05-09 12:47:14 +0200

    (v3.1.11) patch: adjusts base components

--- packages/client/components/base/BaseBtn.vue
+++ packages/client/components/base/BaseBtn.vue
@@ -2,81 +2,92 @@
 
 import type { PropType } from 'vue';
 
+type ButtonSize = 'small' | 'medium' | 'large' | 'x-large';
+
 const props = defineProps({
-	rounded: { 
-		type: String, 
-		default: undefined
-	},
-	to: { 
-		type: String, 
-		default: undefined 
-	},
-	href: { 
-		type: String, 
-		default: undefined 
-	},
-	target: {
-		type: String as PropType<'_blank'>,
-		default: '_blank'
-	},
-	disabled: { 
-		type: Boolean, 
-		default: undefined 
-	}
+  color: {
+    type: String,
+    default: 'primary'
+  },
+  size: {
+    type: String as PropType<ButtonSize>,
+    default: undefined
+  },
+  outlined: {
+    type: Boolean,
+    default: undefined
+  },
+  rounded: {
+    type: String,
+    default: undefined
+  },
+  noHover: {
+    type: Boolean,
+    default: false
+  },
+  to: {
+    type: String,
+    default: undefined
+  },
+  href: {
+    type: String,
+    default: undefined
+  },
+  target: {
+    type: String as PropType<'_blank'>,
+    default: '_blank'
+  },
+  disabled: {
+    type: Boolean,
+    default: undefined
+  }
 });
 
-const emit = defineEmits([ 'click' ]);
+const emit = defineEmits(['click']);
 
 const getClass = computed(() => {
-	const classes = [];
-	
-	// color
-	if(props.disabled){
-		classes.push('bg-slate-400');
-	} else {
-    classes.push('bg-primary');
+  const classes = [];
+
+  // color
+  if (props.disabled) {
+    classes.push('bg-slate-400');
+  } else {
+    classes.push(props.color ? `bg-${props.color}` : '');
   }
 
-  // center text
-  classes.push('text-center');
+  // add rounded class
+  classes.push(props.rounded ? `rounded-${props.rounded}` : 'rounded-lg');
 
-	// add rounded class
-	classes.push(props.rounded ? `rounded-${props.rounded}` : 'rounded-lg');
+  // add some padding
+  classes.push('p-2');
 
-	// add some padding
-	classes.push('p-2');
+  // size
+  if (props.size === 'small') {
+    classes.push('text-sm');
+  } else if (props.size === 'medium') {
+    classes.push('text-lg');
+  } else if (props.size === 'large') {
+    classes.push('text-xl');
+  } else if (props.size === 'x-large') {
+    classes.push('text-2xl');
+  }
 
-	return classes.join(' ');
+  return classes.join(' ');
 });
 
 </script>
 
 <template>
-  <NuxtLink
-    v-if="props.to"
-    :class="getClass"
-    :to="props.disabled ? undefined : props.to"
-  >
+  <NuxtLink v-if="props.to" :class="getClass" :to="props.disabled ? undefined : props.to">
     <slot />
   </NuxtLink>
-  
-  <NuxtLink
-    v-else-if="props.href"
-    :class="getClass"
-    :disabled="props.disabled"
-    :href="props.href"
-    :target="props.target"
-  >
+
+  <NuxtLink v-else-if="props.href" :class="getClass" :disabled="props.disabled" :href="props.href"
+    :target="props.target">
     <slot />
   </NuxtLink>
-  
-  <button
-    v-else
-    :class="getClass"
-    :disabled="props.disabled"
-    :href="props.to"
-    @click="emit('click')"
-  >
+
+  <button v-else :class="getClass" :disabled="props.disabled" :href="props.to" @click="emit('click')">
     <slot />
   </button>
 </template>
