b59f12bb98587445faed8999fc02026a48e9433b
Oliver Manzi <olivermanzi.dev@gmail.com>
2024-05-09 13:46:06 +0200

    (v3.1.14) patch: replaces contact image with performant webp

--- packages/client/pages/contact.vue
+++ packages/client/pages/contact.vue
@@ -1,20 +1,31 @@
 <script setup lang="ts">
 import { usePortfolioStore } from '~/stores/portfolio.store';
+import type { Image } from '~/types';
 
 useSeoSetup({ title: "Contact" });
 
 const portfolioStore = usePortfolioStore();
 
+const getImage = computed<Image>(() => {
+  return {
+    _id: 'contact',
+    src: '/images/contact.webp',
+    caption: 'Contact',
+  };
+});
+
 </script>
 
 <template>
   <base-page>
-    <div class="grid grid-cols-2">
-      <img src="/images/contact.jpg" height="700px" width="500px" />
+    <div class="flex flex-col-reverse md:flex-row md:justify-center gap-2">
+
+      <image-card class="md:w-4/12" :image="getImage" :hide-details="true" :expand="true" />
 
       <div class="p-2 flex flex-col">
         <a class="hide-link" :href="`mailto:${portfolioStore.portfolio.email}`"><b>email</b> me</a>
-        <a v-for="social in portfolioStore.portfolio.socials" :key="social.name" class="hide-link" target="_blank" rel="noreferrer" :href="social.url"><b>visit</b> my {{ social.name }}</a>
+        <a v-for="social in portfolioStore.portfolio.socials" :key="social.name" class="hide-link" target="_blank"
+          rel="noreferrer" :href="social.url"><b>visit</b> my {{ social.name }}</a>
       </div>
     </div>
   </base-page>
