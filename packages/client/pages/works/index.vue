0cd2eaa69cfca81a4d38a172d3b2e294f902d8ac
Oliver Manzi <olivermanzi.dev@gmail.com>
2024-05-10 16:30:35 +0200

    (v3.2.8) patch: sorts projects/works by date (latest first)

--- packages/client/pages/works/index.vue
+++ packages/client/pages/works/index.vue
@@ -10,23 +10,26 @@ const workStore = useWorkStore();
 
 const works = ref<Work[]>([]);
 
+const getSortedWorks = computed(() =>
+	// sort works by date (unix timestamp) latest first
+	works.value.sort((a: Work, b: Work) => Number(b.date) - Number(a.date))
+);
+
 onMounted(async () => {
-  works.value = await workStore.indexWorks();
+	works.value = await workStore.indexWorks();
 });
 </script>
 
 <template>
-  <base-page title="Works">
-    <div v-if="authStore.isAuthenticated" class="mt-2 mb-4">
-      <base-btn color="primary" to="/works/create">
-        Create work
-      </base-btn>
-    </div>
+	<base-page title="Works">
+		<div v-if="authStore.isAuthenticated" class="mt-2 mb-4">
+			<base-btn color="primary" to="/works/create"> Create work </base-btn>
+		</div>
 
-    <div class="mt-2 grid grid-cols-1 md:grid-cols-3 gap-4">
-      <div v-for="work in works" :key="work._id">
-        <work-card :work="work" :admin="authStore.isAuthenticated" />
-      </div>
-    </div>
-  </base-page>
+		<div class="mt-2 grid grid-cols-1 md:grid-cols-3 gap-4">
+			<div v-for="work in getSortedWorks" :key="work._id">
+				<work-card :work="work" :admin="authStore.isAuthenticated" />
+			</div>
+		</div>
+	</base-page>
 </template>
