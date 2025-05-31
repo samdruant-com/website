6760da45b9a724bfc2a184ed5a9fc411022aa478
Oliver Manzi <olivermanzi.dev@gmail.com>
2025-05-31 16:54:36 +0200

    chore(frontend): removes unnecessary deps

--- src/frontend/nuxt.config.ts
+++ src/frontend/nuxt.config.ts
@@ -1,11 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  build: {
-    transpile: [
-      "vue-sonner"
-    ]
-  },
-
   /**
    * Use styling globally in app
    */
